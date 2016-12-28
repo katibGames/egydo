9053c920acccb5642800932efe1577191112ad33
mohamed esam <abdelhady@outlook.com>
2016-12-28 09:38:10 +0200

    styling the todo list
    just added the todo list colors and style

--- newtodo.js
+++ newtodo.js
@@ -1,12 +1,5 @@
 function showdata() {
   var newtodo = $("#newtodo").val();
     $("ol").prepend("<li>" + newtodo +"</l1>");
-  $("#btn2").click(function(){
-        $("ol").$("li").append("<li>newtodo</li>");
-    });
+    
 }
-$(document).ready(function(){
-    $("#btn2").click(function(){
-        $("ol").prepend("<li>Appended item</li>");
-    });
-});
