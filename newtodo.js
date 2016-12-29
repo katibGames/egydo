c2a6886b4bdd5d25fdc51a98da4cd1e75026be31
mohamed esam <abdelhady@outlook.com>
2016-12-29 08:45:41 +0200

    first try to delete ghe added task
    {problem} the option is not yet activated t

--- newtodo.js
+++ newtodo.js
@@ -1,5 +1,13 @@
 function showdata() {
   var newtodo = $("#newtodo").val();
-    $("ol").prepend("<li>" + newtodo +"</l1>");
-    
+    $("ol").prepend("<li>" +
+     newtodo +
+    "<span>"
+    +"&times;"
+    +"</span>"
+     +"</l1>");
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
 }
