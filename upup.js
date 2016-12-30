fbfc5cd73446c97b4a7f8f81072852dcc96ab86b
mohamed esam <abdelhady@outlook.com>
2016-12-30 19:19:24 +0200

    header , offline
    made the website available to run offlice and PWA .
    added logo header to increase the logo value :)

---
+++ upup.js
@@ -0,0 +1,12 @@
+UpUp.start({
+		'content-url': 'index.html',
+		'assets': [
+			'style.css',
+			'main.js',
+			'jquery-3.1.1.js',
+      'manifest.json',
+      'newtodo.js',
+      'egyption code.png'
+      'https://fonts.googleapis.com/icon?family=Material+Icons'
+		]
+	});
