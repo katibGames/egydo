12893b0551b57fe5257e9f5da8532b2f95afc548
mohamed esam <abdelhady@outlook.com>
2016-12-31 22:40:26 +0200

    adding more functions
    this update is about makin the app more flexible and more easy
    adding database for the functions soon and facebook sign in too

--- newtodo.js
+++ newtodo.js
@@ -1,8 +1,14 @@
 function showdata() {
     var newtodo = $("#newtodo").val();
-    $("ol").append('<li class"batodo" >' + newtodo + '<span class="deltodo">' +"&times;" + '</span>' + "</li>");
+    if ($("#newtodo").val() == 0) {   
+    }
+    else {
+      $("ol").append('<li class"batodo" >' + newtodo + '<span class="deltodo">' +"&times;" + '</span>' + "</li>");
+      modal.style.display = "none";
+    }
+
     $(".deltodo").click(function(){
             $(this).parents("li:first").remove();
         });
-    modal.style.display = "none";
+
 };
