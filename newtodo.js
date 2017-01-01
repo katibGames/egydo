d4cf02d474597469e60e04fbdcfcc24e8644524d
mohamed esam <abdelhady@outlook.com>
2017-01-01 11:30:41 +0200

    styling header

--- newtodo.js
+++ newtodo.js
@@ -1,14 +1,13 @@
 function showdata() {
     var newtodo = $("#newtodo").val();
-    if ($("#newtodo").val() == 0) {   
+    if ($("#newtodo").val() == 0) {
     }
     else {
-      $("ol").append('<li class"batodo" >' + newtodo + '<span class="deltodo">' +"&times;" + '</span>' + "</li>");
+      $("#ol").append('<li class"batodo" >' + newtodo + '<span class="deltodo">' +"&times;" + '</span>' + "</li>");
       modal.style.display = "none";
     }
 
     $(".deltodo").click(function(){
             $(this).parents("li:first").remove();
         });
-
 };
