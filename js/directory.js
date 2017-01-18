001f98df9cb20d38cf14d853f2ff7c3854c42617
mohamed esam <abdelhady@outlook.com>
2017-01-18 22:58:24 +0200

    Revert "Revert "a new way in adding task with more care to arabic lang""
    This reverts commit 0a09ee8ab35cf257da9e48f7d88d2fb57df7462b.

--- js/directory.js
+++ js/directory.js
@@ -1,31 +1,33 @@
 angular.module('egydo', []).controller('tasklist', function ($scope) {
-    $scope.list = [{
-        name: 'صلاة الصبح'
-        , time: 10
-        }, {
-        name: 'افطر'
-        , time: 12
-        }, {
-        name: 'اشرب شاى'
-        , time: 11
-        }, {
-        name: 'ميعاد النزول'
-        , time: 13
-        }];
-    $scope.addtask = function () {
-        $scope.list.push({
-            name: $scope.name
-            , time: $scope.time
-        , });
-        $scope.name = '';
-        $scope.time = 0;
+    var dirlist = this;
+    dirlist.list = [{
+        name: 'صلاة الصبح',
+        time: 10
+    }, {
+        name: 'افطر',
+        time: 12
+    }, {
+        name: 'اشرب شاى',
+        time: 11
+    }, {
+        name: 'ميعاد النزول',
+        time: 13
+    }];
+    dirlist.toggle = false;
+    dirlist.addtask = function () {
+        dirlist.list.push({
+            name: dirlist.name,
+            time: dirlist.time,
+        });
+        dirlist.name = '';
+        dirlist.time = 0;
     };
-    $scope.remove = function (item) {
-        $scope.list.splice($scope.list.indexOf(item),1)
+    dirlist.remove = function (item) {
+        dirlist.list.splice(dirlist.list.indexOf(item), 1)
     };
-    $scope.edit = function (item) {
+    dirlist.edit = function (item) {
         $scope.current = item
     };
-    $scope.current = {} ;
-     
+    $scope.current = {};
+
 })
